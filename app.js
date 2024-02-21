d97c0a8d043f7bbab619e074baca8c0a9ca19df6
pmkhanh <pmkhanha2@gmail.com>
2024-02-21 16:21:34 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const contacsRouter = require("./app/routers/contact.route");
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contacsRouter);
 
 app.get("/", (req, res)=> {
     res.json({ message: "Welcome to contact book application."})
