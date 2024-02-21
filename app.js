771f1855ad6fca179a5d8b278219c827bc49fbba
pmkhanh <pmkhanha2@gmail.com>
2024-02-21 16:50:57 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const ApiError = require("./app/api-error");
 
 const app = express();
 const contacsRouter = require("./app/routers/contact.route");
@@ -10,6 +11,17 @@ app.use("/api/contacts", contacsRouter);
 
 app.get("/", (req, res)=> {
     res.json({ message: "Welcome to contact book application."})
+});
+
+app.use("/api/contacts", contacsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
 });
 
 module.exports = app;
